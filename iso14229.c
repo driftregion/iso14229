1374b05e6fcb565dd4771cdf294b350c75ef26f8
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2023-03-10 08:56:51 +0800

    use uint32_t instead of uint16_t for LinuxSockTpOpen CAN IDs

--- iso14229.c
+++ iso14229.c
@@ -231,8 +231,8 @@ static int LinuxSockBind(const char *if_name, uint32_t rxid, uint32_t txid) {
     return fd;
 }
 
-static int LinuxSockTpOpen(UDSTpHandle_t *hdl, const char *if_name, uint16_t phys_rxid,
-                           uint16_t phys_txid, uint32_t func_rxid, uint32_t func_txid) {
+static int LinuxSockTpOpen(UDSTpHandle_t *hdl, const char *if_name, uint32_t phys_rxid,
+                           uint32_t phys_txid, uint32_t func_rxid, uint32_t func_txid) {
     assert(if_name);
     UDSTpLinuxIsoTp_t *impl = (UDSTpLinuxIsoTp_t *)hdl->impl;
     hdl->recv = tp_recv;
