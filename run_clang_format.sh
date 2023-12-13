77f77fcc8a1c6baa1eea5e967d775a07a908b611
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2023-12-12 21:15:03 -0700

    fix run_clang_format.sh

--- run_clang_format.sh
+++ run_clang_format.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-files=`find src -type f \( -name '*.c' -o -name '*.h'\) -not -path "src/tp/isotp-c/*"`
+files=`find src -type f \( -name '*.c' -o -name '*.h' \) -not -path "src/tp/isotp-c/*"`
 
 for file in $files ; do
     if [ -z "$CHECK_FORMAT" ] ; then
