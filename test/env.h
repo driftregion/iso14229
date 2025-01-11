4b7835b85a15567509ed726e76a2fcde8f403912
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2025-01-11 16:01:54 -0700

    merge server 0x10 tests

--- test/env.h
+++ test/env.h
@@ -9,23 +9,6 @@
 #include <setjmp.h>  // needed for cmocka
 #include <cmocka.h>
 
-enum ENV_TpType {
-    ENV_TP_TYPE_MOCK,       // tp/mock.h
-    ENV_TP_TYPE_ISOTP_SOCK, // tp/isotp_sock.h
-    ENV_TP_TYPE_ISOTPC,     // tp/isotp_c_socketcan.h
-};
-
-/**
- * @brief Environment options
- */
-typedef struct {
-    enum ENV_TpType tp_type; // transport type
-    const char *ifname;      // CAN interface name used for isotp_sock and socketcan
-    uint32_t srv_src_addr, srv_target_addr, srv_src_addr_func;
-    uint32_t cli_src_addr, cli_target_addr, cli_tgt_addr_func;
-    bool assert_no_client_err;
-} ENV_Opts_t;
-
 typedef struct {
     UDSServer_t *server;
     UDSClient_t *client;
@@ -33,17 +16,7 @@ typedef struct {
     UDSTpHandle_t *client_tp;
 } Env_t;
 
-/**
- * @brief return a transport configured as client
- * @return UDSTpHandle_t*
- */
-UDSTpHandle_t *ENV_TpNew(const char *name);
-void ENV_TpFree(UDSTpHandle_t *tp);
-void ENV_RegisterServer(UDSServer_t *server);
-void ENV_RegisterClient(UDSClient_t *client);
-void ENV_AttachHook(void (*fn)(void *), void *arg);
 void EnvRunMillis(Env_t *env, uint32_t millis);
-
 
 #define _TEST_INT_COND(a, b, cond)                                                                 \
     {                                                                                              \
