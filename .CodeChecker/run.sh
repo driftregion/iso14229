f017975892947f9e45e550408cb37911bcfef19c
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2025-02-03 11:16:37 -0700

    add MISRA suppressions list

--- .CodeChecker/run.sh
+++ .CodeChecker/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-CodeChecker analyze compile_commands.json \
-    --ignore .CodeChecker/skipfile_bazel.txt \
-    -o reports
+# CodeChecker analyze compile_commands.json \
+#     --ignore .CodeChecker/skipfile_bazel.txt \
+#     -o reports
 
 
 # Make the MISRA report
-# CodeChecker analyze compile_commands.json \
-#     --ignore .CodeChecker/skipfile_bazel.txt \
-#     --ignore .CodeChecker/skipfile_thirdparty.txt \
-#     --cppcheckargs .CodeChecker/cppcheckargs.txt \
-#     --analyzer-config cppcheck:addons=.cppcheck/misra.json \
-#     --analyzer-config cppcheck:platform=unix64 \
-#     -o misra_reports 
+CodeChecker analyze compile_commands.json \
+    --ignore .CodeChecker/skipfile_bazel.txt \
+    --ignore .CodeChecker/skipfile_thirdparty.txt \
+    --cppcheckargs .CodeChecker/cppcheckargs.txt \
+    --analyzer-config cppcheck:addons=.cppcheck/misra.json \
+    --analyzer-config cppcheck:platform=unix64 \
+    -o misra_reports 
