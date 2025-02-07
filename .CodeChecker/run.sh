9f92ec80399fcf55dcc9071ce0946bc808430582
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2025-02-06 23:20:00 -0700

    move MISRA checking to cppcheck invocation

--- .CodeChecker/run.sh
+++ .CodeChecker/run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# CodeChecker analyze compile_commands.json \
-#     --ignore .CodeChecker/skipfile_bazel.txt \
-#     -o reports
-
-
-# Make the MISRA report
 CodeChecker analyze compile_commands.json \
     --ignore .CodeChecker/skipfile_bazel.txt \
-    --ignore .CodeChecker/skipfile_thirdparty.txt \
-    --cppcheckargs .CodeChecker/cppcheckargs.txt \
-    --analyzer-config cppcheck:addons=.cppcheck/misra.json \
-    --analyzer-config cppcheck:platform=unix64 \
-    -o misra_reports 
+    -o reports
+
+# MISRA checking via CodeChecker isn't working properly. Use ./.cppcheck/run.sh instead.
+# CodeChecker analyze compile_commands.json \
+#     --ignore .CodeChecker/skipfile_bazel.txt \
+#     --ignore .CodeChecker/skipfile_thirdparty.txt \
+#     --cppcheckargs .CodeChecker/cppcheckargs.txt \
+#     --analyzer-config cppcheck:addons=.cppcheck/misra.json \
+#     --analyzer-config cppcheck:platform=unix64 \
+#     -o misra_reports 
