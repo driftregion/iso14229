5f84f376f9328b5a84131a0d835dcd2c4d0a127c
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2025-08-22 01:21:26 +0100

    remove test_log_disable from coverage test

--- tools/coverage_run.sh
+++ tools/coverage_run.sh
@@ -6,4 +6,8 @@ bazel coverage \
 --instrument_test_targets \
 --experimental_collect_code_coverage_for_generated_files \
 --test_output=errors \
-//test:all
+//test:test_server_tp_mock \
+//test:test_client_tp_mock \
+//test:test_tp_isotp_compliance_c \
+//test:test_tp_isotp_compliance_sock \
+//test:test_tp_isotp_compliance_mock
