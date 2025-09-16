d16c5a02d9ddd87a9fd6c81f40b9164bd25633c3
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2025-09-16 20:33:57 +0000

    update scripts and corpus

--- scripts/1_run_fuzzer.sh
+++ scripts/1_run_fuzzer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bazel run -c opt --config=asan-libfuzzer //fuzz:fuzz_server_run -- \
+bazel run -c opt --config=hermetic-fuzz //fuzz:fuzz_server_run -- \
 --timeout_secs=10 \
 --fuzzing_output_root=$(bazel info workspace)/fuzz/outputs
