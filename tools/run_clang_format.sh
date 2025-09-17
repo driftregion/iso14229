a0f6a66a7645d51dda7b58dad59006f50d6071c0
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2025-09-17 04:34:40 +0000

    update run_clang_format.sh to use hermetic toolchain

--- tools/run_clang_format.sh
+++ tools/run_clang_format.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+set -e
+
+CHECK_FORMAT=${CHECK_FORMAT:-""}
 
 files=`find src -type f \( -name '*.c' -o -name '*.h' \) -not -path "src/tp/isotp-c/*"`
 
+# Create a stable launcher to the hermetic clang-format.
+CLANG_FORMAT="${TMPDIR:-/tmp}/clang-format.bazel"
+if [[ ! -x "$CLANG_FORMAT" ]]; then
+  bazel run --script_path="$CLANG_FORMAT" @llvm_toolchain//:clang-format > /dev/null
+fi
+
 for file in $files ; do
+    full_path="$(realpath $file)"
     if [ -z "$CHECK_FORMAT" ] ; then
-        clang-format -i $file
+        bash -c "$CLANG_FORMAT -i $full_path"
     else
-        clang-format -Werror --dry-run $file
+        bash -c "$CLANG_FORMAT -Werror --dry-run $full_path"
     fi
 done
