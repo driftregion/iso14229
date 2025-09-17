61c21ba0a4e5b5ca92b088a311eca2c490dfa2ba
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2025-09-17 18:18:26 +0000

    update git hook to do formatting

--- tools/CodeChecker/run.sh
+++ tools/CodeChecker/run.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
+# Unified CodeChecker analysis: clang-tidy, clangsa, and MISRA (cppcheck) all together
 CodeChecker analyze compile_commands.json \
     --ignore tools/CodeChecker/skipfile_bazel.txt \
-    -o reports
-
-# MISRA checking via CodeChecker isn't working properly. Use tools/cppcheck/run.sh instead.
-# CodeChecker analyze compile_commands.json \
-#     --ignore tools/CodeChecker/skipfile_bazel.txt \
-#     --ignore tools/CodeChecker/skipfile_thirdparty.txt \
-#     --cppcheckargs tools/CodeChecker/cppcheckargs.txt \
-#     --analyzer-config cppcheck:addons=tools/cppcheck/misra.json \
-#     --analyzer-config cppcheck:platform=unix64 \
-#     -o misra_reports 
+    --ignore tools/CodeChecker/skipfile_thirdparty.txt \
+    --file "*/iso14229.*" \
+    --analyzer-config cppcheck:addons=tools/cppcheck/misra.json \
+    --analyzer-config cppcheck:platform=unix64 \
+    --analyzer-config cppcheck:cc-verbatim-args-file=tools/CodeChecker/cppcheckargs.txt \
+    -o reports 
