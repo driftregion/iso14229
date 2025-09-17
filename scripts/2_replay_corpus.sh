fec1bd0e103f49ff5ce0bca3bac1a04e17341865
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2025-09-17 01:11:11 +0000

    normalize paths so that coverage is consolidated

--- scripts/2_replay_corpus.sh
+++ scripts/2_replay_corpus.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 # Run coverage with hermetic toolchain
 bazel coverage -c opt --config=hermetic-coverage //fuzz:fuzz_server
 cp -f "$(bazel info output_path)/_coverage/_coverage_report.dat" coverage.lcov
-# sed -E -i 's#^SF:bazel-out/.*/iso14229.c#SF:iso14229.c#' coverage.lcov
-# sed -E -i 's#^SF:bazel-out/.*/iso14229.h#SF:iso14229.h#' coverage.lcov
+sed -E -i 's#^SF:bazel-out/.*/iso14229.c#SF:iso14229.c#' coverage.lcov
+sed -E -i 's#^SF:bazel-out/.*/iso14229.h#SF:iso14229.h#' coverage.lcov
 ls -l coverage.lcov
