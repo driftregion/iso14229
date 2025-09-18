ede1f437686a9b5448c38e6bbe39e571ee82f269
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2025-09-18 18:00:15 +0000

    remove MISRA checking from SA

--- tools/CodeChecker/run.sh
+++ tools/CodeChecker/run.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Unified CodeChecker analysis: clang-tidy, clangsa, and MISRA (cppcheck) all together
 CodeChecker analyze compile_commands.json \
     --ignore tools/CodeChecker/skipfile_bazel.txt \
     --ignore tools/CodeChecker/skipfile_thirdparty.txt \
     --file "iso14229.c" \
-    --analyzer-config cppcheck:addons=tools/cppcheck/misra.json \
-    --analyzer-config cppcheck:platform=unix64 \
-    --analyzer-config cppcheck:cc-verbatim-args-file=tools/CodeChecker/cppcheckargs.txt \
     -o reports 
