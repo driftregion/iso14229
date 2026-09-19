965a2efd995e0c43669e9c55a85c910c2b0a8c80
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2026-09-19 11:57:52 -0700

    sync CI invocations with .bazelrc config defs

--- scripts/1_run_fuzzer.sh
+++ scripts/1_run_fuzzer.sh
@@ -3,7 +3,7 @@
 # Get workspace path
 WORKSPACE=$(bazel info workspace)
 
-bazel run -c opt --config=hermetic-fuzz //fuzz:fuzz_server_bin -- \
+bazel run -c opt --config=fuzz //fuzz:fuzz_server_bin -- \
 -max_total_time=600 \
 -artifact_prefix=${WORKSPACE}/fuzz/outputs/ \
 -jobs=64 \
